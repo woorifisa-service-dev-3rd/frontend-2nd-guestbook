abcadcff3ed308d47d4b800fcfb3d6d819ea8d7d
guguttemy <gugutetmy@gmail.com>
2024-07-24 11:32:15 +0900

    #3. Todo 조회 기능 구현

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,37 @@
+import { useState } from 'react'
 import TodoBody from './components/todos/TodoBody'
 import TodoHeader from './components/todos/TodoHeader'
 import DefaultLayout from './layouts/DefaultLayout'
 
+const dummyTodos = [
+  {
+    id: 1,
+    title: 'React 공부',
+    summary: 'React를 공부한다.',
+    category: 'TODO',
+  },
+  {
+    id: 2,
+    title: '점심 먹기',
+    summary: '점심을 먹는다.',
+    category: 'PROGRESS',
+  },
+  {
+    id: 3,
+    title: '커피 마시기',
+    summary: '커피를 마신다.',
+    category: 'DONE',
+  }
+]
+
 function App() {
+
+  const [todos, setTodos] = useState(dummyTodos);
 
   return (
     <>
       <DefaultLayout>
         <header>
-        
           <div className="flex justify-center">
             <a to="/" className='flex'>
               <h1 className='py-8 text-red-200 max-w-max text-7xl'>todos
@@ -19,7 +42,7 @@ function App() {
         </header>
         <section className="max-w-xl m-4 mx-auto">
           <TodoHeader />
-          <TodoBody />
+          <TodoBody todos={todos}/>
         </section>
       </DefaultLayout>
     </>
--- src/components/todos/TodoBody.jsx
+++ src/components/todos/TodoBody.jsx
@@ -1,12 +1,10 @@
 import React from 'react'
 import TodoItem from './TodoItem'
 
-const todos = [1, 2, 3];
-const TodoBody = () => {
+const TodoBody = ({ todos }) => {
   return (
     <ul className='px-0 my-8'>
-        {/* li태그를 todos 배열의 요소만큼 렌더링 */}
-        {todos.map(todo => <TodoItem key={todo}/>)}
+        {todos.map(todo => <TodoItem todo={todo} key={todo.id}/>)}
     </ul>
   )
 }
