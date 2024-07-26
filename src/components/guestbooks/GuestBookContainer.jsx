827afad2ad3e53c4b7fa6e33fb614628c5b49abf
jihyuk0414 <dealon2580@naver.com>
2024-07-26 15:07:10 +0900

    feat: add 기능 추가

--- src/components/guestbooks/GuestBookContainer.jsx
+++ src/components/guestbooks/GuestBookContainer.jsx
@@ -8,38 +8,40 @@ import { GuestBookContextForData, GuestBookContextForMethod } from '../contexts/
 
 const GuestBookContainer = () => {
 
-    // const [guestbooks, setguestbooks] = useState(dummyGuestBooks);
-    // // console.log("게스트북", guestbooks);
+    const [guestbooks, setGuestbooks] = useState(dummyGuestBooks);
+    console.log("게스트북", guestbooks);
 
 
-    // const dummyguestbooks = dummyGuestBooks
+    const dummyguestbooks = dummyGuestBooks
 
-    // const getCurrentDateTime = () => {
-    //     const now = new Date();
+    const getCurrentDateTime = () => {
+        const now = new Date();
       
-    //     const year = now.getFullYear();
-    //     const month = String(now.getMonth() + 1).padStart(2, '0'); 
-    //     const day = String(now.getDate()).padStart(2, '0');
-    //     const hours = String(now.getHours()).padStart(2, '0');
-    //     const minutes = String(now.getMinutes()).padStart(2, '0');
-    //     const seconds = String(now.getSeconds()).padStart(2, '0');
+        const year = now.getFullYear();
+        const month = String(now.getMonth() + 1).padStart(2, '0'); 
+        const day = String(now.getDate()).padStart(2, '0');
+        const hours = String(now.getHours()).padStart(2, '0');
+        const minutes = String(now.getMinutes()).padStart(2, '0');
+        const seconds = String(now.getSeconds()).padStart(2, '0');
 
-    //     return `${year}-${month}-${day} ${hours}:${minutes}:${seconds}`;
-    //   }
+        return `${year}-${month}-${day} ${hours}:${minutes}:${seconds}`;
+      }
 
-    // const addGuestBookHandler = () => {
+    const addGuestBookHandler = ({nickname,content}) => {
+
+        console.log("여기서닉네임",nickname)
     
-    //     const newGuestBook = {
-    //         id : self.crypto.randomUUID(),
-    //         nickname,
-    //         content,
-    //         dateTime: getCurrentDateTime()
-    //     }
+        const newGuestBook = {
+            id : self.crypto.randomUUID(),
+            nickname,
+            content,
+            dateTime: getCurrentDateTime()
+        }
 
-    //     setguestbooks ([...guestbooks, newGuestBook])
+        setGuestbooks ([...guestbooks, newGuestBook])
 
 
-    // }
+    }
    
 
 
@@ -47,8 +49,8 @@ const GuestBookContainer = () => {
   return (
     <section className='max-w-xl m-6 mx-auto border  border-gray-700 rounded-lg w-full my-[30px] p-[30px] bg-custom-blue'>
 
-        <GuestBookContextForData.Provider>
-            <GuestBookContextForMethod.Provider>
+        <GuestBookContextForData.Provider value = {guestbooks}>
+            <GuestBookContextForMethod.Provider value = {{addGuestBookHandler}}>
             <h1 className='text-center text-2xl font-bold mb-[24px]'>방명록</h1>
             <GuestBookHeader />
             <GuestBookBody />
